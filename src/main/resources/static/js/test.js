6e723945c126090c59aaf6afb0ca388a4b0f546e
Ted Zamarron <ted.zamarron@gmail.com>
2020-06-26 18:02:01 -0500

    adding spells

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -7,22 +7,25 @@ $(document).ready(function () {
             // console.log("Found results: ");
             // console.log(array);
             array.forEach((item) => {
-                console.log(item.name);
-                console.log(item.desc);
-                console.log(item.equipment);
-                console.log(item.hit_dice);
-                console.log(item.hp_at_1st_level);
-                console.log(item.prof_armor);
-                console.log(item.prof_saving_throws);
-                console.log(item.prof_skills);
-                console.log(item.prof_tools);
-                console.log(item.prof_weapons);
-                console.log(item.spellcasting_ability);
-                console.log(item.subtypes_names);
-                console.log(item.table);
+                if (item.name === "Warlock") {
+                    console.log("Name:");
+                    console.log(item.name);
+                    console.log("")
+                    console.log(item.desc);
+                    console.log(item.equipment);
+                    console.log(item.hit_dice);
+                    console.log(item.hp_at_1st_level);
+                    console.log(item.prof_armor);
+                    console.log(item.prof_saving_throws);
+                    console.log(item.prof_skills);
+                    console.log(item.prof_tools);
+                    console.log(item.prof_weapons);
+                    console.log(item.spellcasting_ability);
+                    console.log(item.subtypes_names);
+                    console.log(item.table);
+                }
             });
         });
     };
     onlineData("classes");
-
 })
