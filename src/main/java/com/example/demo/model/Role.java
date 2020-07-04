0037a12e9d580844ae165565b4c63bcd6b3e5df2
Ted Zamarron <ted.zamarron@gmail.com>
2020-07-03 20:30:42 -0500

    added role and type models

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,61 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String hitDice;
+
+    @Column
+    private String hitPoints;
+
+    @Column
+    private String armor;
+
+    @Column
+    private String weapon;
+
+    @Column
+    private String tool;
+
+    @Column
+    private String savingThrow;
+
+    public Role() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getHitDice() {
+        return hitDice;
+    }
+
+    public String getHitPoints() {
+        return hitPoints;
+    }
+
+    public String getArmor() {
+        return armor;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public String getTool() {
+        return tool;
+    }
+
+    public String getSavingThrow() {
+        return savingThrow;
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Type.java
@@ -0,0 +1,66 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String abilityScore;
+
+    @Column
+    private String speed;
+
+    @Column
+    private boolean darkvision;
+
+    @Column
+    private String alignment;
+
+    @Column
+    private String size;
+
+    @Column
+    private String languages;
+
+    public Type() {
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAbilityScore(String abilityScore) {
+        this.abilityScore = abilityScore;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public void setDarkvision(boolean darkvision) {
+        this.darkvision = darkvision;
+    }
+
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+}
